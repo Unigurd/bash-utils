ac859c815b245eb850ea9dcdbaa89d46f7fd7030
Sigurd Dam Sonniks <Sigurddam@hotmail.com>
2024-01-15 23:35:32 +0100

    flake.nix works again

--- flake.nix
+++ flake.nix
@@ -5,9 +5,10 @@
   outputs = {self, nixpkgs}:
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in {
-      packages.x86_65-linux = {
+      packages.x86_64-linux = {
         label = import ./label.nix pkgs;
         note  = import ./note.nix pkgs;
+        default = import ./label.nix pkgs; # Should provide all packages I guess
       };
     };
 }
