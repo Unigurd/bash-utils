205a66b0cb78abe532cfe9362278971ccb05dd3e
Sigurd Dam Sonniks <Sigurddam@hotmail.com>
2024-01-15 23:31:11 +0100

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,13 +1,13 @@
 {
-  inputs =
-    {
-      nixpkgs.url = "github:nixos/nixpkgs";
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
   outputs = {self, nixpkgs}:
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in
-      {
+    in {
+      packages.x86_65-linux = {
         label = import ./label.nix pkgs;
         note  = import ./note.nix pkgs;
       };
+    };
 }
