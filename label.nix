92e15abe3260a763091a7da4b83b49790875d6c3
Sigurd Dam Sonniks <Sigurddam@hotmail.com>
2024-01-19 23:13:25 +0100

    Remove install-label.sh wrapper

--- label.nix
+++ label.nix
@@ -5,18 +5,18 @@ let
   # Adds coreutils to PATH if somehow not present
   # to ensure the script works.
   # OUT is substituted with whatever $out is.
-  wrapper = ''
-    exists() {
-      type "$1" >/dev/null 2>/dev/null
-    }
+  # wrapper = ''
+  #   exists() {
+  #     type "$1" >/dev/null 2>/dev/null
+  #   }
 
-    if exists mkdir && exists cat && exists echo
-    then true
-    else PATH="${pkgs.coreutils}/bin:$PATH"
-    fi
+  #   if exists mkdir && exists cat && exists echo
+  #   then true
+  #   else PATH="${pkgs.coreutils}/bin:$PATH"
+  #   fi
 
-    source OUT/label.sh
-  '';
+  #   source OUT/label.sh
+  # '';
 
 in
 mkDerivation {
@@ -25,11 +25,8 @@ mkDerivation {
   completion = ./label-completion.sh;
   dontUnpack = true;
   installPhase = ''
-    mkdir -p $out/bin
-    cp $src $out/label.sh
-    echo '${wrapper}' > $out/bin/install-label.sh
-    sed -i "s|OUT|$out|g" "$out"/bin/install-label.sh
-    chmod 555 "$out"/bin/install-label.sh
+    mkdir -p "$out/bin"
+    cp $src "$out/bin/gurd-label.sh"
 
     mkdir -p "$out"/share/bash-completion/completions
     cp "$completion" "$out"/share/bash-completion/completions
