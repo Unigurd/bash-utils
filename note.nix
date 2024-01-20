c1f10085c252194e220bf061623ba65d2a8a34bc
Sigurd Dam Sonniks <Sigurddam@hotmail.com>
2024-01-21 00:56:23 +0100

    Simplify note installPhase

--- note.nix
+++ note.nix
@@ -9,9 +9,6 @@ pkgs.stdenv.mkDerivation {
     mkdir -p $out/lib
     cp $src/note $out/lib/note
     cp $src/note-dir $out/lib/note-dir
-    # sed -i "s|note-dir|$out/lib/note-dir|g" $out/lib/note
-    chmod 555 $out/lib/note
-    chmod 555 $out/lib/note-dir
     makeWrapper $out/lib/note $out/bin/note --prefix PATH : ${pkgs.coreutils}
     makeWrapper $out/lib/note-dir $out/bin/note-dir --prefix PATH : ${pkgs.coreutils}
   '';
