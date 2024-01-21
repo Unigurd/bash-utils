dbe20ebed719827936843b2677cf3e472621b8a1
Sigurd Dam Sonniks <Sigurddam@hotmail.com>
2024-01-21 22:07:03 +0100

    Don't accidentally add note-dir as note-grep

--- note.nix
+++ note.nix
@@ -9,7 +9,7 @@ pkgs.stdenv.mkDerivation {
     mkdir -p $out/lib
     cp $src/note $out/lib/note
     cp $src/note-dir $out/lib/note-dir
-    cp $src/note-dir $out/lib/note-grep
+    cp $src/note-grep $out/lib/note-grep
     makeWrapper $out/lib/note $out/bin/note --prefix PATH : ${pkgs.coreutils}
     makeWrapper $out/lib/note-dir $out/bin/note-dir --prefix PATH : ${pkgs.coreutils}
     makeWrapper $out/lib/note-grep $out/bin/note-grep --prefix PATH : ${pkgs.coreutils}
